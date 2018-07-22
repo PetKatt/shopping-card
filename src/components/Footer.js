080c08cb05b46a5a9e9b07b3b021944ec0406340
PetKatt <piotrwier@o2.pl>
2018-07-22 13:24:08 +0200

    Footer component: DONE

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Footer = () => (
+	<footer>
+		Done by 
+		<a 
+			href="https://github.com/PetKatt" 
+			rel="noopener noreferrer" 
+			target="_blank" 
+			title="Access LinkedIn">
+		PetKatt</a>
+	</footer>
+);
+
+export default Footer;
