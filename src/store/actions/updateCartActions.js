0c51acf73b2da847868a6a4b7fb6935d91220b70
PetKatt <piotrwier@o2.pl>
2018-08-04 11:29:46 +0200

    persistentCart.js DONE; & updateCartActions: Building...

--- src/store/actions/updateCartActions.js
+++ src/store/actions/updateCartActions.js
@@ -4,5 +4,29 @@ import persistentCart from "../../persistentCart";
 
 
 export const updateCart = (cartProducts) => dispatch => {
-	let productQuantity
+	let productQuantity = cartProducts.reduce((sum, p) => {
+		sum += p.quantity;
+		return sum;
+	}, 0);
+
+	let totalPrice = cartProducts.reduce((sum, p) => {
+		sum += p.price * p.quantity;
+		return sum;
+	}, 0);
+
+	let installments = cartProducts.reduce((greater, p) => {
+		greater = p.installments > greater ? p.installments : greater;
+		return greater;
+	}, 0);
+
+
+	let cartTotals = {
+		productQuantity,
+		installments,
+		totalPrice,
+		currencyId: "BRL",
+		currencyFormat: "R$"
+	};
+
+	persistentCart()
 };
